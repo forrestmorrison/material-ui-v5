a0a852d3f1172122b36b943fc12db5c62194c42d
forrestmorrison <forrestmorrison@gmail.com>
2022-12-23 15:45:41 -0600

    added Grid

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import { Container } from '@mui/system';
+import { Grid } from '@mui/material';
 import TourCard from './components/TourCard';
 
 function App() {
   return (
     <div className="App">
-      <TourCard />
+      <Container>
+        <Grid container spacing={2}>
+          <Grid item xs={3}>
+            <TourCard />
+          </Grid>
+          <Grid item xs={3}>
+            <TourCard />
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 }
