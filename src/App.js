ec78c6e80e9d0454f57f1fb61519d81ff9d75b14
forrestmorrison <forrestmorrison@gmail.com>
2022-12-26 11:12:18 -0600

    changed favicon & app title

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 import './App.css';
 import { Container } from '@mui/system';
-import { Grid } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
 
 import SearchAppBar from './components/AppBar';
 import TourCard from './components/TourCard';
+
+import cities from "./data.json"
 
 function App() {
   return (
     <div className="App">
       <SearchAppBar />
       <Container sx={ { marginY: 5 } }>
-        <Grid container spacing={2}>
+        {cities.map((city) => (
+          <>
+            <Typography 
+              variant='h4'
+              component='h2'
+              marginTop={5}
+              marginBottom={3}
+            >
+              Top {city.name} Tours
+            </Typography>
+          </>
+        ))}
+        {/* <Grid container spacing={2}>
           <TourCard />
           <TourCard />
           <TourCard />
           <TourCard />
-        </Grid>
+        </Grid> */}
       </Container>
     </div>
   );
